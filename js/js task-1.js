715289118bc342acb291978dc9514f81d3517a07
MichaelPopov62 <romantikzizni@gmail.com>
2025-03-07 12:33:30 +0200

    add changed Js

---
+++ js/js task-1.js
@@ -0,0 +1,17 @@
+"use stript";
+
+//tack 1
+// оголошення функціі з параметрами quantity та pricePerDroid 
+function makeTransaction(quantity, pricePerDroid) {
+
+  // разраховуємо загальну вартість замовлення
+  const totalPrice = quantity * pricePerDroid;
+
+  // Повертаємо повідомлення про покупку
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+
+// викликаемо функцію для перевірки іі роботи
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
